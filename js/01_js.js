d03251df7d0489f4243e0d0ce6fb9a1cb911b651
dEmOnCoDeR169 <pradyumna1809@gmail.com>
2025-12-01 21:56:59 +0530

    finally

--- js/01_js.js
+++ js/01_js.js
@@ -1,1 +1,6 @@
-console.log("JS is runnnig");
+fetch("https://cool-website-back.onrender.com")
+  .then(response => response.json())
+  .then(data => {
+      document.body.innerHTML += `<p>Backend says: ${data.message}</p>`;
+  })
+  .catch(err => console.error("Fetch error:", err));
